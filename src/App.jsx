edeca0403035a50c009169ae69fe28c048b6c2e2
divya-lad <125095134+divya-lad@users.noreply.github.com>
2023-11-30 23:35:58 +0000

    Create bounce effect on social media icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Divya Lad",
+  title: "Web Developer",
+  email: "mistrydivya19@yahoo.com",
+  gitHub: "divya-lad",
+  instagram: "divya_lad",
+  linkedIn: "divya-lad",
+  medium: "divya-lad",
+  twitter: "divya_lad",
+  youTube: "divya-lad",
 };
 
 const primaryColor = "#4E567E";
